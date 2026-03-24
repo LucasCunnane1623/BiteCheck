7eacef37822952ebf8827873f1b579419a753e7a
LucasCunnane1623 <lucascunnane16@gmail.com>
2026-03-24 13:18:06 -0400

    deleted all React code as we're not allowed to use it for frontend. Client folder is empty now

---
+++ server/database/db.js
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+import settings from '../config/settings.js';
+
+let client = null;
+let db = null;
+
+export const connect = async () => {
+    if (db) {
+        return db;
+    }
+    try {
+        client = await MongoClient.connect(settings.mongo.url);
+        db = client.db(settings.mongo.dbname);
+        await db.dropDatabase();
+        await db.collection('restaurants').createIndex({location: '2dsphere'});
+        await db.collection('restaurants').createIndex({name: 'text', cuisine: 'text'});
+        console.log(`Connected to MongoDB': ${settings.mongo.dbname}`);
+        return db;
+
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1);
+    }
+}
+
+export const  getdb = () => {
+    if (!db) {
+        throw new Error('Database not connected. Call connect() first.');
+    }
+    return db;
+};
+
+export const close = async () => {
+    if (client){
+        await client.close();
+        db = null;
+        client = null;
+    }
+}
---
+++ server/services/authService.js
@@ -0,0 +1,51 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import settings from '../config/settings.js';
+import { getdb } from '../database/db.js';
+
+export const registerUser = async (email, password, username) => {
+    const db = await getdb();
+    const collection = db.collection('users');
+
+    const existingUser = await collection.findOne({
+        email
+    });
+    
+    if (existingUser) {
+        throw new Error('Email already in use');
+    };
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await collection.insertOne({
+        email,
+        username, 
+        password: hashedPassword,
+        createdAt: new Date()
+    });
+
+    return result.insertedId;
+};
+
+export const loginUser = async (email, password) => {
+    const db = await getdb();
+    const user = await db.collection('users').findOne({ email});
+
+    if (!user){
+        throw new Error("Invalid credentials");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch){
+        throw new Error("Invalid credentials");
+    }
+
+    const token = jwt.sign(
+        { userId: user._id, email: user.email},
+        process.env.JWT_SECRET || settings.secret,
+        { expiresIn: '7d' }
+    )
+
+    return {token, username: user.username};
+}

8fe87ecc9a09bc813b70942cf59d2b55e561838e
Lucas Cunnane <lucascunnane@moflo.ai>
2026-04-02 22:18:40 -0400

    added some more UI elements

--- src/routes/landingRoutes.js
+++ src/routes/landingRoutes.js
@@ -13,9 +13,9 @@ const router = Router();
  */
 router.get('/', async (req, res)=>{
     return res.render('landing',{
-        title: "Bitecheck: Welcome",
-        header: "Welcome To Bitecheck",
-        body: "<p>Yo gang this is where the description is</p>"
+        title: "BiteCheck: Welcome",
+        header: "Welcome To Bitecheck!",
+        body: "<p><b>BiteCheck</b> is a public health–focused dining transparency platform designed to provide New Yorkers and visitors with reliable access to restaurant inspection data. By integrating directly with official restaurant inspection databases, BiteCheck delivers up-to-date information on health and safety compliance across the city. Our platform empowers users to make informed dining decisions by clearly presenting health inspection results, including documented violations related to food preparation practices, sanitation standards, and workplace hygiene. Through an intuitive and user-friendly interface, diners can easily review a restaurant’s inspection history before choosing where to eat. BiteCheck’s mission is to elevate food safety to the same level of importance as cuisine, price, and location. By increasing transparency and accessibility of public health data, we aim to help reduce the risk of food-borne illness, promote higher standards within the restaurant industry, and foster a safer dining environment for both residents and tourists.</p>"
     });
 });
 
